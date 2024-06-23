2996535235d0c5ba265e7615b41f2b520053ee83
Daris02 <hei.raymond.2@gmail.com>
2024-06-23 15:23:36 +0300

    fix: update attendance interval

--- src/main/java/com/hei/app/scoring/Attendance.java
+++ src/main/java/com/hei/app/scoring/Attendance.java
@@ -1,8 +1,38 @@
 package com.hei.app.scoring;
 
-public record Attendance (
-    int day,
-    String interval
-    // private int begin;
-    // private int end;
-) {}
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import lombok.Data;
+
+@Data
+public class Attendance {
+    private int day;
+    private LocalTime begin;
+    private LocalTime end;
+
+    public Attendance(int day, String interval) {
+        this.day = day;
+        String[] parts = interval.split("-");
+        if (parts.length != 2) throw new RuntimeException("Intervalle format not valid : " + interval);
+        this.begin = LocalTime.parse(parts[0], DateTimeFormatter.ofPattern("HH:mm"));
+        this.end = LocalTime.parse(parts[1], DateTimeFormatter.ofPattern("HH:mm"));
+    }
+
+    public int getWorkHours() {
+        Duration duration = Duration.between(begin, end);
+        long durationInHours = duration.toHours();
+        return (int) durationInHours;
+    }
+
+    public int getNightWorkHours() {
+        Duration duration = Duration.between(begin, end);
+        long durationInHours = duration.toHours();
+        return (int) durationInHours;
+    }
+
+    public boolean containsNightHours() {
+        return begin.isAfter(LocalTime.of(18, 0)) || end.isBefore(LocalTime.of(6, 0));
+    }
+}
